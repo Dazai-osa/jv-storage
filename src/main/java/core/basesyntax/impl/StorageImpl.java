e983d9ff43d959562aac70f2ec013e3e848a4af1
Andrew <reznichenko.a200212@gmail.com>
2026-07-12 17:09:14 +0300

    try7

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -14,38 +14,38 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
    private int currentSize = 0;
 
-    @Override
-    public void put(K key, V value) {
-        int index = indexOf(key);
-        if (index >= 0) {
-            values[index] = value;
-        } else if (index == -1 && currentSize < MAX_SIZE) {
-            keys[currentSize] = key;
-            values[currentSize] = value;
-            currentSize++;
-        }
-    }
+   @Override
+   public void put(K key, V value) {
+       int index = indexOf(key);
+       if (index >= 0) {
+           values[index] = value;
+       } else if (index == -1 && currentSize < MAX_SIZE) {
+           keys[currentSize] = key;
+           values[currentSize] = value;
+           currentSize++;
+       }
+   }
 
-    private int indexOf(K key) {
-        for (int i = 0; i < currentSize; i++) {
-            if (key.equals(keys[i])) {
-                return i;
-            }
-        }
-        return -1;
-    }
+   private int indexOf(K key) {
+       for (int i = 0; i < currentSize; i++) {
+           if (key.equals(keys[i])) {
+               return i;
+           }
+       }
+       return -1;
+   }
 
-    @Override
-    public V get(K key) {
-        int index = indexOf(key);
-        if (index >= 0) {
-            return values[index];
-        }
-        return null;
-    }
+   @Override
+   public V get(K key) {
+       int index = indexOf(key);
+       if (index >= 0) {
+           return values[index];
+       }
+       return null;
+   }
 
-    @Override
-    public int size() {
-        return currentSize;
-    }
+   @Override
+   public int size() {
+       return currentSize;
+   }
 }
